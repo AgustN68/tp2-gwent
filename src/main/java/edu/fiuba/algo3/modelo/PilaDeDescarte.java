b55affee09f69b8ba12bf9656b9e1e2835d2d288
axeel25 <abillordo@fi.uba.ar>
2025-06-01 16:43:31 -0300

    Primeros metodos para el Objeto descarte

--- src/main/java/edu/fiuba/algo3/modelo/PilaDeDescarte.java
+++ src/main/java/edu/fiuba/algo3/modelo/PilaDeDescarte.java
@@ -1,4 +1,26 @@
 package edu.fiuba.algo3.modelo;
+import edu.fiuba.algo3.modelo.Carta;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PilaDeDescarte {
+    private List<Carta> descartes;
+    public PilaDeDescarte(){
+        this.descartes = new ArrayList<>();
+    }
+
+     public void descartarUna(Carta carta){
+        this.descartes.add(carta);
+    }
+
+    public void descartarCartas(List<Carta> descartes){
+        this.descartes.addAll(descartes);
+    }
+
+    public void mostrarDescartes(){
+       // this.descartes.mostrar(); No esta implementado en cartas.
+    }
 }
