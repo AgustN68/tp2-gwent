2d3a171c9ee824bc1c29139f3bbcb3eadfdf9b3a
Santiago Fardini <89415821+FARDOO@users.noreply.github.com>
2025-06-01 23:21:15 -0300

    Añadimos implementación de sección

--- src/main/java/edu/fiuba/algo3/modelo/Seccion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Seccion.java
@@ -1,8 +1,28 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seccion {
 
-    public void reducirPuntaje(int valor){
+    private List<Unidad> cartasUnidades;
 
+    public Seccion() {
+        cartasUnidades = new ArrayList<>();
     }
+    
+    public void reducirPuntaje(int valor){}
+    
+    public void ubicar(Unidad unidad){
+        cartasUnidades.add(unidad);
+    }
+
+    public Puntaje puntajeTotal() {
+        Puntaje total = new Puntaje(0);
+        for (Unidad unidad : cartasUnidades) {
+            total = total.sumar(unidad);
+        }
+        return total;
+    }
+
 }
