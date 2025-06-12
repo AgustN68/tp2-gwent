e5d75dd01f2d87401c00aed56c2f15851f30914a
Tomás Conti <tconti@fi.uba.ar>
2025-06-12 17:36:30 -0300

    Agregados tests a Jugador y Tablero

--- src/main/test/edu/fiuba/algo3/modelo/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/JugadorTest.java
@@ -8,19 +8,99 @@ import edu.fiuba.algo3.modelo.Seccion.CuerpoACuerpo;
 import edu.fiuba.algo3.modelo.Seccion.Rango;
 import edu.fiuba.algo3.modelo.Seccion.Seccion;
 
+import edu.fiuba.algo3.modelo.exceptions.NoHayCartasSuficientesException;
+import edu.fiuba.algo3.modelo.exceptions.NoHayCartasUnidadSuficientesException;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
 public class JugadorTest {
     @Test
-    public void test01UnJugadorPuedeSeleccionarUnaUnidad() {
+    public void test01UnJugadorPuedeTomarUnaCartaDelMazo() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+
+        //"Act"
+        jugador.tomarCartasMazo(1);
+
+        //"Assert"
+        assertEquals(1, jugador.verMano().size());
+    }
+
+    @Test
+    public void test02UnJugadorPuedeTomarDosCartasDelMazo() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+
+        //"Act"
+        jugador.tomarCartasMazo(2);
+
+        //"Assert"
+        assertEquals(2, jugador.verMano().size());
+    }
+
+    @Test
+    public void test03UnJugadorPuedeTomarTresCartasDelMazo() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+
+        //"Act"
+        jugador.tomarCartasMazo(3);
+
+        //"Assert"
+        assertEquals(3, jugador.verMano().size());
+    }
+
+    @Test
+    public void test04UnJugadorPuedeSeleccionarUnaUnidad() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+        jugador.tomarCartasMazo(1);
+
+        //"Act"
+        Carta cartaSeleccionada = jugador.seleccionarCarta(0);
+
+        //"Assert"
+        assertEquals(Unidad.class, cartaSeleccionada.getClass());
+    }
+
+    @Test
+    public void test05UnJugadorPuedeSeleccionarUnaUnidad() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+        jugador.tomarCartasMazo(2);
+
+        //"Act"
+        Carta cartaSeleccionada = jugador.seleccionarCarta(1);
+
+        //"Assert"
+        assertEquals(Unidad.class, cartaSeleccionada.getClass());
+    }
+
+    @Test
+    public void test06UnJugadorPuedeObtenerSuPuntaje() {
         //"Arrange"
         Seccion cuerpoACuerpo = new CuerpoACuerpo();
         Seccion rango = new Rango();
@@ -31,18 +111,101 @@ public class JugadorTest {
         secciones.add(rango);
         secciones.add(asedio);
 
-        Tablero tablero = new Tablero(secciones);
+        Jugador jugador = new Jugador(new Tablero(secciones), mock(Mazo.class), new PilaDeDescarte());
 
+        new Unidad(cuerpoACuerpo, 4, new SinModificador()).ubicar(cuerpoACuerpo);
+        new Unidad(cuerpoACuerpo, 2, new SinModificador()).ubicar(cuerpoACuerpo);
+
+        //"Act"
+        Puntaje puntajeObtenido = jugador.obtenerPuntaje();
+
+        //"Assert"
+        assertEquals(6, puntajeObtenido.obtenerValor());
+    }
+
+    @Test
+    public void test07UnJugadorPuedeObtenerSuPuntaje() {
+        //"Arrange"
+        Seccion cuerpoACuerpo = new CuerpoACuerpo();
+        Seccion rango = new Rango();
+        Seccion asedio = new Asedio();
+
+        List<Seccion> secciones = new ArrayList<>();
+        secciones.add(cuerpoACuerpo);
+        secciones.add(rango);
+        secciones.add(asedio);
+
+        Jugador jugador = new Jugador(new Tablero(secciones), mock(Mazo.class), new PilaDeDescarte());
+
+        new Unidad(cuerpoACuerpo, 7, new SinModificador()).ubicar(cuerpoACuerpo);
+        new Unidad(cuerpoACuerpo, 8, new SinModificador()).ubicar(cuerpoACuerpo);
+
+        //"Act"
+        Puntaje puntajeObtenido = jugador.obtenerPuntaje();
+
+        //"Assert"
+        assertEquals(15, puntajeObtenido.obtenerValor());
+    }
+
+    @Test
+    public void test08UnJugadorPuedeVerificarSiTieneCartasSuficientesEnSuMano() {
+        //"Arrange"
         Mazo mazoUnidades = mock(Mazo.class);
         when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
 
-        Jugador jugador = new Jugador(tablero, mazoUnidades, new PilaDeDescarte());
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+
+        jugador.tomarCartasMazo(5);
+
+        //Act y Assert
+        jugador.validarCartasSuficientesMano(5);
+    }
+
+    @Test
+    public void test09UnJugadorPuedeVerificarSiTieneCartasSuficientesEnSuMano() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+
+        jugador.tomarCartasMazo(9);
+
+        //Act y Assert
+        jugador.validarCartasSuficientesMano(7);
+    }
+
+    @Test
+    public void test10UnJugadorPuedeVerificarSiNoTieneCartasSuficientesEnSuMano() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+
         jugador.tomarCartasMazo(1);
 
-        //"Act"
-        Carta cartaSeleccionada = jugador.seleccionarCarta(0);
+        //Act y Assert
+        assertThrows(
+                NoHayCartasSuficientesException.class,
+                ()-> jugador.validarCartasSuficientesMano(5)
+        );
+    }
 
-        //"Assert"
-        assertEquals(Unidad.class, cartaSeleccionada.getClass());
+    @Test
+    public void test11UnJugadorPuedeVerificarSiNoTieneCartasSuficientesEnSuMano() {
+        //"Arrange"
+        Mazo mazoUnidades = mock(Mazo.class);
+        when(mazoUnidades.obtenerCarta()).thenReturn(new Unidad(new CuerpoACuerpo(), 7, new SinModificador()));
+
+        Jugador jugador = new Jugador(new Tablero(new ArrayList<>()), mazoUnidades, new PilaDeDescarte());
+
+        jugador.tomarCartasMazo(13);
+
+        //Act y Assert
+        assertThrows(
+                NoHayCartasSuficientesException.class,
+                ()-> jugador.validarCartasSuficientesMano(14)
+        );
     }
 }
--- src/main/test/edu/fiuba/algo3/modelo/TableroTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/TableroTest.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.Carta.Especial.Clima;
 import edu.fiuba.algo3.modelo.Carta.Unidad;
 import edu.fiuba.algo3.modelo.Modificador.SinModificador;
 import edu.fiuba.algo3.modelo.Seccion.Asedio;
@@ -29,11 +30,154 @@ public class TableroTest {
 
         Tablero tablero = new Tablero(secciones);
 
-        //"Act"
         new Unidad(cuerpoACuerpo, 4, new SinModificador()).ubicar(cuerpoACuerpo);
         new Unidad(cuerpoACuerpo, 2, new SinModificador()).ubicar(cuerpoACuerpo);
+        //"Act"
+        Puntaje puntajeObtenido = tablero.calcularPuntaje();
 
         //"Assert"
-        assertEquals(6, tablero.calcularPuntaje().obtenerValor());
+        assertEquals(6, puntajeObtenido.obtenerValor());
+    }
+
+    @Test
+    public void test02UnTableroPuedeCalcularSuPuntajeTotal(){
+        //"Arrange"
+        Seccion cuerpoACuerpo = new CuerpoACuerpo();
+        Seccion rango = new Rango();
+        Seccion asedio = new Asedio();
+
+        List<Seccion> secciones = new ArrayList<>();
+        secciones.add(cuerpoACuerpo);
+        secciones.add(rango);
+        secciones.add(asedio);
+
+        Tablero tablero = new Tablero(secciones);
+
+        new Unidad(cuerpoACuerpo, 3, new SinModificador()).ubicar(cuerpoACuerpo);
+        new Unidad(cuerpoACuerpo, 9, new SinModificador()).ubicar(cuerpoACuerpo);
+        //"Act"
+        Puntaje puntajeObtenido = tablero.calcularPuntaje();
+
+        //"Assert"
+        assertEquals(12, puntajeObtenido.obtenerValor());
+    }
+
+    @Test
+    public void test03UnTableroPuedeLimpiarElClima(){
+        //"Arrange"
+        Seccion cuerpoACuerpo1 = new CuerpoACuerpo();
+        List<Seccion> seccionesTablero1 = new ArrayList<>();
+        seccionesTablero1.add(cuerpoACuerpo1);
+        Tablero tablero1 = new Tablero(seccionesTablero1);
+
+        Seccion cuerpoACuerpo2 = new CuerpoACuerpo();
+        List<Seccion> seccionesTablero2 = new ArrayList<>();
+        seccionesTablero2.add(cuerpoACuerpo2);
+        Tablero tablero2 = new Tablero(seccionesTablero2);
+
+        Clima clima = new Clima(cuerpoACuerpo1, cuerpoACuerpo2);
+
+        new Unidad(cuerpoACuerpo1, 4, new SinModificador()).ubicar(cuerpoACuerpo1);
+        new Unidad(cuerpoACuerpo1, 2, new SinModificador()).ubicar(cuerpoACuerpo1);
+
+        clima.usar();
+
+        //"Act"
+        tablero1.limpiarClima();
+
+        //"Assert"
+        assertEquals(6, tablero1.calcularPuntaje().obtenerValor());
+    }
+
+    @Test
+    public void test04UnTableroPuedeRemoverLasUnidadesMasFuertes(){
+        //"Arrange"
+        Seccion cuerpoACuerpo = new CuerpoACuerpo();
+        Seccion rango = new Rango();
+        Seccion asedio = new Asedio();
+        List<Seccion> seccionesTablero = new ArrayList<>();
+        seccionesTablero.add(cuerpoACuerpo);
+        seccionesTablero.add(rango);
+        seccionesTablero.add(asedio);
+        Tablero tablero = new Tablero(seccionesTablero);
+
+        new Unidad(cuerpoACuerpo, 8, new SinModificador()).ubicar(cuerpoACuerpo);
+        new Unidad(asedio, 4, new SinModificador()).ubicar(asedio);
+        new Unidad(rango, 3, new SinModificador()).ubicar(rango);
+
+        //"Act"
+        tablero.removerUnidadesMasFuertes();
+
+        //"Assert"
+        assertEquals(7, tablero.calcularPuntaje().obtenerValor());
+    }
+
+    @Test
+    public void test05UnTableroPuedeRemoverLasUnidadesMasFuertes(){
+        //"Arrange"
+        Seccion cuerpoACuerpo = new CuerpoACuerpo();
+        Seccion rango = new Rango();
+        Seccion asedio = new Asedio();
+        List<Seccion> seccionesTablero = new ArrayList<>();
+        seccionesTablero.add(cuerpoACuerpo);
+        seccionesTablero.add(rango);
+        seccionesTablero.add(asedio);
+        Tablero tablero = new Tablero(seccionesTablero);
+
+        new Unidad(cuerpoACuerpo, 4, new SinModificador()).ubicar(cuerpoACuerpo);
+        new Unidad(asedio, 8, new SinModificador()).ubicar(asedio);
+        new Unidad(rango, 3, new SinModificador()).ubicar(rango);
+
+        //"Act"
+        tablero.removerUnidadesMasFuertes();
+
+        //"Assert"
+        assertEquals(7, tablero.calcularPuntaje().obtenerValor());
+    }
+
+    @Test
+    public void test06UnTableroPuedeRemoverLasUnidadesMasFuertes(){
+        //"Arrange"
+        Seccion cuerpoACuerpo = new CuerpoACuerpo();
+        Seccion rango = new Rango();
+        Seccion asedio = new Asedio();
+        List<Seccion> seccionesTablero = new ArrayList<>();
+        seccionesTablero.add(cuerpoACuerpo);
+        seccionesTablero.add(rango);
+        seccionesTablero.add(asedio);
+        Tablero tablero = new Tablero(seccionesTablero);
+
+        new Unidad(cuerpoACuerpo, 4, new SinModificador()).ubicar(cuerpoACuerpo);
+        new Unidad(asedio, 8, new SinModificador()).ubicar(asedio);
+        new Unidad(rango, 9, new SinModificador()).ubicar(rango);
+
+        //"Act"
+        tablero.removerUnidadesMasFuertes();
+
+        //"Assert"
+        assertEquals(12, tablero.calcularPuntaje().obtenerValor());
+    }
+
+    @Test
+    public void test07UnTableroPuedeRemoverLasUnidadesMasFuertes(){
+        //"Arrange"
+        Seccion cuerpoACuerpo = new CuerpoACuerpo();
+        Seccion rango = new Rango();
+        Seccion asedio = new Asedio();
+        List<Seccion> seccionesTablero = new ArrayList<>();
+        seccionesTablero.add(cuerpoACuerpo);
+        seccionesTablero.add(rango);
+        seccionesTablero.add(asedio);
+        Tablero tablero = new Tablero(seccionesTablero);
+
+        new Unidad(cuerpoACuerpo, 4, new SinModificador()).ubicar(cuerpoACuerpo);
+        new Unidad(asedio, 9, new SinModificador()).ubicar(asedio);
+        new Unidad(rango, 9, new SinModificador()).ubicar(rango);
+
+        //"Act"
+        tablero.removerUnidadesMasFuertes();
+
+        //"Assert"
+        assertEquals(4, tablero.calcularPuntaje().obtenerValor());
     }
 }
