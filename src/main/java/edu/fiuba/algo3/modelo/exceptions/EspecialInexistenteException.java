4ed4a2ac5adb4301815cde942024c64498082ea8
Tomás Conti <tconti@fi.uba.ar>
2025-06-27 17:03:37 -0300

    Quitado EspecialInexistenteException.java, no se usaba

--- src/main/java/edu/fiuba/algo3/modelo/exceptions/EspecialInexistenteException.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo.exceptions;
-
-public class EspecialInexistenteException extends RuntimeException {
-    public EspecialInexistenteException(String message) {
-        super(message);
-    }
-}
