6b896ed96d81b88410199c884e04312759ee446e
Tomás Conti <tconti@fi.uba.ar>
2025-06-27 15:02:38 -0300

    Arreglado clima Despejado Por una falla en el constructor, se creaba como un clima regular, en vez de Despejado

--- src/main/java/edu/fiuba/algo3/modelo/Factory/EspecialFactory.java
+++ src/main/java/edu/fiuba/algo3/modelo/Factory/EspecialFactory.java
@@ -12,7 +12,7 @@ public class EspecialFactory {
     private static final String TIPO_TIERRA_ARRASADA = "Tierra arrasada";
     private static final String TIPO_MORALE_BOOST = "Morale Boost";
     private static final String TIPO_CLIMA = "Clima";
-    private static final String NOMBRE_CLIMA_DESPEJADO = "Tiempo Despejado";
+    private static final String NOMBRE_CLIMA_DESPEJADO = "Tiempo despejado";
 
     public static Especial crearEspecial(
             String tipo,
