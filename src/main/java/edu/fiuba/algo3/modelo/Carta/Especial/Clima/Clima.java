fa46290b1500f144713c7fa687e29a281cc90a3c
Tomás Conti <tconti@fi.uba.ar>
2025-06-27 16:18:42 -0300

    Quitado import de clase que fue borrada

--- src/main/java/edu/fiuba/algo3/modelo/Carta/Especial/Clima/Clima.java
+++ src/main/java/edu/fiuba/algo3/modelo/Carta/Especial/Clima/Clima.java
@@ -2,7 +2,6 @@ package edu.fiuba.algo3.modelo.Carta.Especial.Clima;
 
 import edu.fiuba.algo3.modelo.Carta.Especial.Especial;
 import edu.fiuba.algo3.modelo.Seccion.Seccion;
-import edu.fiuba.algo3.modelo.exceptions.SeccionInvalidaException;
 
 import java.util.List;
 
